df4fa81d6863c409bf76407e1cee74047d5d3dc7
Rimantas <belauzas@gmail.com>
2023-05-29 15:06:16 +0300

    Numbers

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+
+Esminiai konceptai programavime:
+
+1) kintamieji - informacijos atsiminimas
+2) if'ai - info palyginimas
+3) loop'ai - operaciju vykdymas N kartu
+4) functions - pasikartojancios logikos aprasymas
+
+*/
--- js/02-numbers.js
+++ js/02-numbers.js
@@ -1,2 +1,88 @@
 console.clear();
 
+/*
+NUMBERS - skaiciai
+
+Sveikieji/desimtainiai
+Teigiami/neigiami
+Mistiniai: Begalybes ir NaN (not-a-number)
+
+Aritmetiniai operatoriai:
++, -, *, /
+
+Salia matematikos:
+(, )
+
+Kintamuju iniciavimo budai:
+const - default
+let - naudoti, tik jei reikia keisti reksmes
+var - senove, todel niekada nenaudoti, nebent TIKRAI zinai ka darai
+
+*/
+
+console.log(1);
+console.log(2);
+console.log(3.14);
+console.log(2.72);
+
+console.log(-1);
+console.log(-2);
+console.log(-3.14);
+console.log(-2.72);
+
+console.log(Infinity);
+console.log(-Infinity);
+
+console.log(NaN);
+
+console.log(2 + 2, 4);
+console.log(2 + 2 * 2, 6);
+console.log((2 + 2) * 2, 8);
+
+console.log(0.1 + 0.2, 0.3);
+
+console.clear();
+
+// KINTAMIEJI
+
+const amzius = 20;
+console.log(amzius);
+
+const saldainiuKiekis = 19;
+const saldainiu_kiekis = 18;
+const SALDAINIU_KIEKIS = 17;
+
+console.log(saldainiuKiekis);
+console.log(saldainiu_kiekis);
+console.log(SALDAINIU_KIEKIS);
+
+console.log(amzius);
+
+console.clear();
+// Kintamieji: konstantos ir kintamieji;
+
+let pinige = 0;
+console.log(pinige);
+console.log(pinige);
+console.log(pinige);
+console.log(pinige);
+
+pinige = 5;
+console.log(pinige);
+console.log(pinige);
+console.log(pinige);
+console.log(pinige);
+
+pinige = 20;
+console.log(pinige);
+console.log(pinige);
+console.log(pinige);
+
+pinige = 4 + 3;
+console.log(pinige);
+
+pinige = pinige + 3;
+console.log(pinige);
+
+pinige = pinige * 4;
+console.log(pinige);
