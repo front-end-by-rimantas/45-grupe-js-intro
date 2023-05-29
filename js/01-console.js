e8000b8f51dae47b678149ae7c21157b8f125cee
Rimantas <belauzas@gmail.com>
2023-05-29 14:24:11 +0300

    Comments

--- js/01-console.js
+++ js/01-console.js
@@ -4,3 +4,24 @@ console.log(1);
 console.log(2);
 console.log(3);
 console.log(4, 5, 6, 7);
+
+// Single-line comment: ctrl + /
+
+// Multi-line
+// as
+// single-line
+// comment
+
+/* Single-line as multi-line comment */
+
+/* Single-line
+as
+multi-line
+comment */
+
+/*
+    Single-line
+    as
+    multi-line
+    comment
+*/
