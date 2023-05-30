9c88d5bc4ef101f5b87cf6fce7e7e9d12526fcd1
Rimantas <belauzas@gmail.com>
2023-05-30 13:43:44 +0300

    String

---
+++ js/03-string.js
@@ -0,0 +1,30 @@
+console.clear();
+
+/*
+STRING - tekstiniu simboliu grandinele, aka tekstas
+
+Iniciavimo budai:
+- viengubos kabutes ('')
+- dvygubos kabutes ("")
+*/
+
+const vardas = 'Jonas';
+const vardas2 = "Maryte";
+
+console.log(vardas);
+console.log(vardas2);
+
+
+const firstName = 'Ona';
+const lastName = 'Onaityte';
+
+// Ona Onaityte
+const fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+// Sveiki, as esu Ona!
+// Sveiki, as esu Jonas!
+// Sveiki, as esu Maryte!
+const rekiam = 'Sveiki, as esu ' + firstName + '!';
+
+console.log(rekiam);
