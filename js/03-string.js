3b978ea0f8018336f832c61c485f673e3b34ae8b
Rimantas <belauzas@gmail.com>
2023-05-30 15:12:14 +0300

    String ND

--- js/03-string.js
+++ js/03-string.js
@@ -28,3 +28,118 @@ console.log(fullName);
 const rekiam = 'Sveiki, as esu ' + firstName + '!';
 
 console.log(rekiam);
+
+console.log('---------------------------');
+
+// Vienguba kabute (').
+const kabute1 = "Vienguba kabute (').";
+console.log(kabute1);
+
+// Dviguba kabute (").
+const kabute2 = 'Dviguba kabute (").';
+console.log(kabute2);
+
+// Vienguba (') ir dviguba (") kabutes.
+// Vienguba (') ir dvi
+// guba (") kabutes.
+const kabute12 = "Vienguba (') ir dvi" + 'guba (") kabutes.';
+console.log(kabute12);
+
+// Vienguba (') ir dviguba (") kabutes.
+const kabute12_1 = 'Vienguba (\') ir dviguba (") kabutes.';
+const kabute12_2 = "Vienguba (') ir dviguba (\") kabutes.";
+const kabute12_3 = "Vienguba (\') ir dviguba (\") kabutes.";
+const kabute12_4 = 'Vienguba (\') ir dviguba (\") kabutes.';
+console.log(kabute12_1);
+console.log(kabute12_2);
+console.log(kabute12_3);
+console.log(kabute12_4);
+
+// Komentaras (//).
+const comment1 = 'Komentaras (//).';
+const comment2 = "Komentaras (//).";
+console.log(comment1);
+console.log(comment2);
+
+// Backslash (\).
+const back1 = 'Backslash (\\).';
+const back2 = "Backslash (\\\\).";
+console.log(back1);
+console.log(back2);
+
+/*
+Labas
+rytas,
+Lietuva!
+*/
+
+// \n - new line (slenkasi eilute zemyn, bet lieka X-asy ten pat)
+// \r - return (grizta i tos pacios eilutes prieki)
+// \t - tab
+
+const labas = 'Labas\n\
+rytas,\n\
+Lietuva!';
+console.log(labas);
+
+const HTML = '\
+<header>\r\n\
+\t<img src="#" alt="Logo" class="logo">\r\n\
+\t<nav>\r\n\
+\t\t<a href="#">Link</a>\r\n\
+\t\t<a href="#">Link</a>\r\n\
+\t\t<a href="#">Link</a>\r\n\
+\t\t<a href="#">Link</a>\r\n\
+\t</nav>\r\n\
+</header>';
+console.log(HTML);
+
+
+console.clear();
+
+// Vienguba (') ir dviguba (") kabutes.
+const kabute3 = `Vienguba (') ir dviguba (") kabutes.`;
+console.log(kabute3);
+
+// Vienguba ('), dviguba (") ir backtick (`) kabutes.
+
+const kabute4 = `Vienguba ('), dviguba (") ir backtick (\`) kabutes.`;
+console.log(kabute4);
+
+const backtickHTML = `<header>
+    <img src="#" alt="Logo" class="logo">
+    <nav>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+    </nav>
+</header>`;
+console.log(backtickHTML);
+
+const name = 'Martynas';
+const age = 99;
+const iLike1 = 'masinos';
+const iLike2 = 'gamta';
+const iLike3 = 'oro balionai';
+
+console.clear();
+
+// Sveiki, mano vardas [VARDAS], man yra [METAI] metai ir man labai patinka [KAS PATINKA], [KAS PATINKA] ir [KAS PATINKA]!
+
+const martynas = "Sveiki, mano vardas " + name + ", man yra " + age + " metai ir man labai patinka " + iLike1 + ", " + iLike2 + " ir " + iLike3 + "!";
+console.log(martynas);
+console.log();
+
+const martis = `Sveiki, mano vardas ${name}, man yra ${age} metai ir man labai patinka ${iLike1}, ${iLike2} ir ${iLike3}!`;
+console.log(martis);
+
+// 5 * 1 = 2
+// 5 * 2 = 4
+// 5 * 3 = 6
+// ...
+// 5 * 10 = 20
+
+const lentele1 = '';
+const lentele2 = ``;
+
