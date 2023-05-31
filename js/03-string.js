622a6533f3d7189f44df3550f007e89d0cc796c4
Rimantas <belauzas@gmail.com>
2023-05-31 13:46:06 +0300

    Strings

--- js/03-string.js
+++ js/03-string.js
@@ -6,6 +6,9 @@ STRING - tekstiniu simboliu grandinele, aka tekstas
 Iniciavimo budai:
 - viengubos kabutes ('')
 - dvygubos kabutes ("")
+
+Ilgis:
+'stringas'.length
 */
 
 const vardas = 'Jonas';
@@ -134,12 +137,119 @@ console.log();
 const martis = `Sveiki, mano vardas ${name}, man yra ${age} metai ir man labai patinka ${iLike1}, ${iLike2} ir ${iLike3}!`;
 console.log(martis);
 
-// 5 * 1 = 2
-// 5 * 2 = 4
-// 5 * 3 = 6
-// ...
-// 5 * 10 = 20
+console.clear();
 
-const lentele1 = '';
-const lentele2 = ``;
 
+const daugiklis = 5;
+const n1 = 1;
+const n2 = 2;
+const n3 = 3;
+const n4 = 4;
+const n5 = 5;
+const n6 = 6;
+const n7 = 7;
+const n8 = 8;
+const n9 = 9;
+const n10 = 10;
+
+const lentele1 = daugiklis + ' * ' + n1 + ' = ' + (daugiklis * n1) + '\r\n' + daugiklis + ' * ' + n2 + ' = ' + (daugiklis * n2) + '\r\n' + daugiklis + ' * ' + n3 + ' = ' + (daugiklis * n3) + '\r\n' + daugiklis + ' * ' + n4 + ' = ' + (daugiklis * n4) + '\r\n' + daugiklis + ' * ' + n5 + ' = ' + (daugiklis * n5) + '\r\n' + daugiklis + ' * ' + n6 + ' = ' + (daugiklis * n6) + '\r\n' + daugiklis + ' * ' + n7 + ' = ' + (daugiklis * n7) + '\r\n' + daugiklis + ' * ' + n8 + ' = ' + (daugiklis * n8) + '\r\n' + daugiklis + ' * ' + n9 + ' = ' + (daugiklis * n9) + '\r\n' + daugiklis + ' * ' + n10 + ' = ' + (daugiklis * n10);
+
+console.log(lentele1);
+console.log();
+
+const lentele2 = `${daugiklis} * ${n1} = ${daugiklis * n1}
+${daugiklis} * ${n2} = ${daugiklis * n2}
+${daugiklis} * ${n3} = ${daugiklis * n3}
+${daugiklis} * ${n4} = ${daugiklis * n4}
+${daugiklis} * ${n5} = ${daugiklis * n5}
+${daugiklis} * ${n6} = ${daugiklis * n6}
+${daugiklis} * ${n7} = ${daugiklis * n7}
+${daugiklis} * ${n8} = ${daugiklis * n8}
+${daugiklis} * ${n9} = ${daugiklis * n9}
+${daugiklis} * ${n10} = ${daugiklis * n10}`;
+
+console.log(lentele2);
+
+console.clear();
+
+const multiplier = 3;
+let number = 0;
+
+const m1 = multiplier * n1;
+const m2 = multiplier * n2;
+
+const lentele3 = `
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+`;
+
+console.log(lentele3);
+
+let sum = 0;
+let index = 0;
+
+const lentele4 = `
+${multiplier} * ${++index} = ${sum += multiplier}
+${multiplier} * ${++index} = ${sum += multiplier}
+${multiplier} * ${++index} = ${sum += multiplier}
+${multiplier} * ${++index} = ${sum += multiplier}
+${multiplier} * ${++index} = ${sum += multiplier}
+${multiplier} * ${++index} = ${sum += multiplier}
+${multiplier} * ${++index} = ${sum += multiplier}
+${multiplier} * ${++index} = ${sum += multiplier}
+${multiplier} * ${++index} = ${sum += multiplier}
+${multiplier} * ${++index} = ${sum += multiplier}
+`;
+
+console.log(lentele4);
+
+console.clear();
+
+const empty = '     gg      ';
+console.log(empty.length);
+//                     11111
+//           012345678901234
+const str = 'Pavyzdys';
+console.log(str, 8, '>>>', str.length);
+console.log(str, 8, '>>>', str.lenght);
+
+const raide1 = str[0];
+console.log(raide1, 'P');
+console.log('---------------');
+
+let strIndex = 0;
+
+console.log(str[strIndex++]);
+console.log(str[strIndex++]);
+console.log(str[strIndex++]);
+console.log(str[strIndex++]);
+console.log(str[strIndex++]);
+console.log(str[strIndex++]);
+console.log(str[strIndex++]);
+console.log(str[strIndex++]);
+
+console.log(str[strIndex++]);
+console.log(str[strIndex++]);
+console.log(str[strIndex++]);
+
+console.log(str[-1]);
+console.log(str[-2]);
+console.log(str[-24562]);
+
+console.clear();
+
+// const userFirstName = 'Chuck';
+// const userLastName = 'Norris';
+const userFirstName = 'Pamela';
+const userLastName = 'Anderson';
+
+const userAbbr = `${userFirstName[0]}.${userLastName[0]}.`;
+console.log(userAbbr);
